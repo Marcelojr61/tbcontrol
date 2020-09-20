ea394a1314e83c4be6de08757076b3d61afe06bb
Marcelo Martins <dw.cv.mj@gmail.com>
2020-09-20 09:34:00 -0300

    Feat: Adicionado Notifications

---
+++ src/TBControl.Business/Interfaces/INotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TBControl.Business.Notifications;
+
+namespace TBControl.Business.Interfaces
+{
+    public interface INotifier
+    {
+        void Handle(Notification notification);
+        List<Notification> GetNotifications();
+        bool HasNotification();
+
+    }
+}
---
+++ src/TBControl.Business/Notifications/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBControl.Business.Notifications
+{
+    public class Notification
+    {
+        public Notification(string message)
+        {
+            this.Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
